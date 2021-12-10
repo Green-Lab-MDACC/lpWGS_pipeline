a9eab70a37e6f1efb18a598b84b482e65368f23d
Bryant Sugg <92883998+bnsugg@users.noreply.github.com>
2021-12-10 11:07:50 -0600

    Add files via upload

---
+++ scripts/copywriter.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/R
+R -e "library(CopywriteR)" \
+-e "Sample_ID <- 'DG_517'" \
+-e "bam_location <- file.path(getwd(),'data',Sample_ID, paste(Sample_ID,'sorted_dedup_realign_BQSR.bam',sep='.'))" \
+-e "sample.control <- data.frame(bam_location, bam_location)" \
+-e "bp.param <- SnowParam(workers = 12, type = 'SOCK')" \
+-e "CopywriteR(sample.control = sample.control, destination.folder=file.path(getwd(),'data',Sample_ID), reference.folder= file.path(getwd(),'data','hg19','hg19_100kb_chr'), bp.param)"
