a2611bb70fea51361bc21891494448b7c2c90f4f
Bryant Sugg <92883998+bnsugg@users.noreply.github.com>
2021-12-10 14:05:30 -0600

    Update copywriter.sh

--- scripts/copywriter.sh
+++ scripts/copywriter.sh
@@ -4,4 +4,5 @@ R -e "library(CopywriteR)" \
 -e "bam_location <- file.path(getwd(),'data',Sample_ID, paste(Sample_ID,'sorted_dedup_realign_BQSR.bam',sep='.'))" \
 -e "sample.control <- data.frame(bam_location, bam_location)" \
 -e "bp.param <- SnowParam(workers = 12, type = 'SOCK')" \
--e "CopywriteR(sample.control = sample.control, destination.folder=file.path(getwd(),'data',Sample_ID), reference.folder= file.path(getwd(),'data','hg19','hg19_100kb_chr'), bp.param)"
+-e "CopywriteR(sample.control = sample.control, destination.folder=file.path(getwd(),'data',Sample_ID), reference.folder= file.path(getwd(),'data','hg19','hg19_100kb_chr'), bp.param)" \
+-e "plotCNA(destination.folder = file.path(getwd(),'data',Sample_ID))"
