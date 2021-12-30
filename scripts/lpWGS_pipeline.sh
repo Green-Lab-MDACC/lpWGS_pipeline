a7aeac17fa40e3f957ca0e6ea1b724bfdc20017e
Bryant Sugg <92883998+bnsugg@users.noreply.github.com>
2021-12-30 11:47:58 -0600

    Add files via upload

--- scripts/lpWGS_pipeline.sh
+++ scripts/lpWGS_pipeline.sh
@@ -32,20 +32,29 @@ Sample_ID_R2="${Sample_ID}m22_R2"
 # java -jar $PWD/programs/GenomeAnalysisTK.jar -T PrintReads -R $PWD/data/hg19/ucsc.hg19.fasta -I $PWD/data/${Sample_ID}/${Sample_ID}.sorted_dedup_realign.bam --BQSR $PWD/data/${Sample_ID}/$Sample_ID.sorted_dedup_realign.recal_data.table -o $PWD/data/${Sample_ID}/${Sample_ID}.sorted_dedup_realign_BQSR.bam
 
 #Perform CopywriteR
-# R -e "library(CopywriteR)" \
-# -e "bam_location <- file.path(getwd(),'data',Sample_ID, paste(Sample_ID,'sorted_dedup_realign_BQSR.bam',sep='.'))" \
-# -e "sample.control <- data.frame(bam_location, bam_location)" \
-# -e "bp.param <- SnowParam(workers = 12, type = 'SOCK')" \
-# -e "CopywriteR(sample.control = sample.control, destination.folder=file.path(getwd(),'data',Sample_ID), reference.folder= file.path(getwd(),'data','hg19','hg19_100kb_chr'), bp.param)" \
-# -e "plotCNA(destination.folder = file.path(getwd(),'data',Sample_ID))" \
+R -e "library(CopywriteR)" \
+-e "Sample_ID <- '${Sample_ID}'" \
+-e "bam_location <- file.path(getwd(),'data',Sample_ID, paste(Sample_ID,'sorted_dedup_realign_BQSR.bam',sep='.'))" \
+-e "sample.control <- data.frame(bam_location, bam_location)" \
+-e "bp.param <- SnowParam(workers = 12, type = 'SOCK')" \
+-e "CopywriteR(sample.control = sample.control, destination.folder=file.path(getwd(),'data',Sample_ID), reference.folder= file.path(getwd(),'data','hg19','hg19_100kb_chr'), bp.param)" \
+-e "plotCNA(destination.folder = file.path(getwd(),'data',Sample_ID))"
 
 #Extract seg file
 R -e "Sample_ID <- '${Sample_ID}'" \
 -e "load(file.path(getwd(),'data',Sample_ID,'CNAprofiles','segment.Rdata'))" \
 -e "write.csv(segment.CNA.object[['output']], file = file.path(getwd(),'data',Sample_ID,'CNAprofiles','${Sample_ID}.csv'))"
 
+#Run readCounter
+cd $PWD/programs/hmmcopy_utils
+cmake $PWD/programs/hmmcopy_utils
+make
+cd .. 
+cd ..
+$PWD/programs/hmmcopy_utils/bin/readCounter --window 1000000 --quality 20 $PWD/data/${Sample_ID}/${Sample_ID}.sorted_dedup_realign_BQSR.bam > $PWD/data/${Sample_ID}/${Sample_ID}.wig --chromosome chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY
+
+#Run IchorCNA
+Rscript $PWD/programs/ichorCNA/scripts/runIchorCNA.R --id ${Sample_ID} --WIG $PWD/data/${Sample_ID}/${Sample_ID}.wig --outDir $PWD/data/${Sample_ID} --gcWig $PWD/programs/ichorCNA/inst/extdata/gc_hg19_1000kb.wig --normal 'c(0.5, 0.85, 0.995, 0.999)' --libdir $PWD/programs/ichorCNA
+
 #Modify seg file
 python ./scripts/cnapp_prepper.py ${Sample_ID}
-
-# #Perform CNApp
-# Rscript $PWD/programs/CNApp-master/server.R
